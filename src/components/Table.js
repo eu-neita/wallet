1b5ff15bdf6d31792e371b1d0295eca1de71c7d5
nathan crevelin <neitass.music@gmail.com>
2023-04-13 14:28:30 -0300

    termina requisito 6

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,11 +1,55 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Table extends Component {
   render() {
+    const { data } = this.props;
     return (
-      <div>Table</div>
+      // <div>Table</div>
+      <table>
+        <thead>
+          <tr>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item) => (
+            <tr key={ item.id }>
+              <td>{item.description}</td>
+              <td>{item.tag}</td>
+              <td>{item.method}</td>
+              <td>{item.value}</td>
+              <td>{item.currency}</td>
+              <td>a</td>
+              <td>a</td>
+              <td>a</td>
+              <td>
+                <button>Editar</button>
+                <button>Excluir</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     );
   }
 }
 
-export default Table;
+const mapStateToProps = (state) => ({
+  data: state.wallet.expenses,
+});
+
+Table.propTypes = {
+  data: PropTypes.arrayOf().isRequired,
+};
+
+export default connect(mapStateToProps)(Table);
